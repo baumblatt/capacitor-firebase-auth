d9cb9b499fc796fecd9bf74d14e569b8467cd6ad
Bernado Baumblatt <baumblatt@gmail.com>
2019-02-28 21:02:37 -0300

    Google Provider authentication in Android platform.

--- src/web.ts
+++ src/web.ts
@@ -9,8 +9,8 @@ export class CapacitorFirebaseAuthWeb extends WebPlugin implements CapacitorFire
     });
   }
 
-  async signIn(options: {provider: {providerId: string;}}): Promise<{provider: {providerId: string; displayName?: string; idToken?: string}}> {
-    return Promise.resolve(options);
+  async signIn(options: {provider: {providerId: string;}}): Promise<{providerId: string; displayName?: string; idToken?: string}> {
+    return Promise.resolve({providerId: options.provider.providerId});
   }
 
   async signOut(options: {provider: {providerId: string;}}): Promise<void> {
